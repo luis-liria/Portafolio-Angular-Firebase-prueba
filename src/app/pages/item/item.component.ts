a5c4e5c03abcadbe1dbcb76e2fb07a816f052917
Luis Liria <LuisLiriadeveloper@gmail.com>
2021-06-07 20:28:06 +0200

    final

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoDescripcion } from 'src/app/interfaces/producto-descripcion.interface';
+import { ProductosService } from 'src/app/services/productos.service';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+    producto: ProductoDescripcion;
+    id: string;
+
+  constructor( private route: ActivatedRoute,
+                public productoService: ProductosService ) { }
+
+  ngOnInit() {
+    this.route.params
+    .subscribe( parametros => {
+
+      //console.log(parametros);
+      this.productoService.getProducto(parametros['id'])
+        .subscribe((producto: ProductoDescripcion) => {
+          
+          this.producto= producto;
+          this.id= parametros['id'];
+          console.log(producto);
+
+        })
+    });
+  }
+
+}
